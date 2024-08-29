a1ed78f7785bfb198c83e06c2ffa3f08a7fb5c57
Dan Polasky <dpolasky@umich.edu>
2024-08-29 16:18:43 -0400

    bump to 1.1.6

--- CMD/Program.cs
+++ CMD/Program.cs
@@ -14,7 +14,7 @@ namespace CMD
     public static class Program
     {
         private static CmdSettings CommandLineSettings;
-        private static readonly string version = "1.1.5";
+        private static readonly string version = "1.1.6";
 
         public static int Main(string[] args)
         {
